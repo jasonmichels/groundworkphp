d7eca1c9c8c9c0c108c758cf88e1b6a7c6f3e8bd
Jason Michels <michelsja@icloud.com>
2016-01-08 15:45:44 -0600

    Implemented fast-route as the route provider

--- src/Http/RouteRequest.php
+++ src/Http/RouteRequest.php
@@ -1,11 +1,9 @@
 <?php namespace Groundwork\Http;
 
-use Groundwork\Exceptions\MethodNotAllowed;
-
 /**
  * Class RouteRequest
  *
- * An instance of a route the app will listen form and respond to
+ * An instance of a route
  *
  * @package Groundwork\Http
  * @author Jason Michels <michelsja@gmail.com>
@@ -39,53 +37,5 @@ class RouteRequest
         $this->route = $route;
         $this->callable = $callable;
         $this->method = $method;
-    }
-
-    /**
-     * See if the requested uri and method matches the route
-     *
-     * @param string $requestUri
-     * @param string $requestMethod
-     * @return bool
-     */
-    public function match(string $requestUri, string $requestMethod)
-    {
-        $match = false;
-        $this->matchRoute($requestUri, function () use ($requestMethod, &$match) {
-            $match = $this->matchMethod($requestMethod);
-        });
-
-        return $match;
-    }
-
-    /**
-     * Match the route to the requested uri
-     *
-     * @param string $requestUri
-     * @param callable $callable
-     * @return bool
-     */
-    protected function matchRoute(string $requestUri, callable $callable)
-    {
-        if ($requestUri == $this->route) {
-            call_user_func($callable);
-        }
-
-        return false;
-    }
-
-    /**
-     * Match the method to the requested method
-     *
-     * @param string $requestMethod
-     * @return bool
-     * @throws MethodNotAllowed
-     */
-    protected function matchMethod(string $requestMethod)
-    {
-        if ($requestMethod == $this->method) {
-            return true;
-        }
-        throw new MethodNotAllowed($requestMethod . ' is not allowed for ' . htmlentities($this->route));
     }
 }
